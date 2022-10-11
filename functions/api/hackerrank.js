69561563e7ea548bdf8092c9c768b7d6c12dde7e
Ken Lee <kenova85@gmail.com>
2022-10-11 16:31:24 +0800

    try fix

--- functions/api/hackerrank.js
+++ functions/api/hackerrank.js
@@ -21,13 +21,21 @@
    return response.text();
  }
  
- export async function onRequest() {
+ export async function onRequest(context) {
+    const {
+        request, // same as existing Worker API
+        env, // same as existing Worker API
+        params, // if filename includes [id] or [[path]]
+        waitUntil, // same as ctx.waitUntil in existing Worker API
+        next, // used for middleware or to fetch assets
+        data, // arbitrary space for passing data between middlewares
+      } = context;
    const init = {
      headers: {
        'content-type': 'application/json;charset=UTF-8',
      },
    };
-   const response = await fetch(url, init);
+   const response = await fetch(url);
    const results = await gatherResponse(response);
    return new Response(results, init);
  }
